b44cf72b97bfac9a3bdc4ed8be4f382fba1102af
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2019-04-19 22:35:28 +0500

    add test to 8-queen nw solution and benchmarks

--- chessboard_test.go
+++ chessboard_test.go
@@ -52,3 +52,55 @@ func TestTheEightQueensProblemMySolution(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestTheEightQueensProblemNW(t *testing.T) {
+	pieces := TheEightQueensProblemNW()
+	if len(pieces) != 8 {
+		t.Errorf("solution not found, expected 8 got %v", len(pieces))
+	}
+
+	// convert pos to chessboard pos
+	toChessPos := func(i, j int) string {
+		return string([]byte{byte(i) + 'a' - 1, byte(j) + '0'})
+	}
+
+	chessPieces := make([]ChessPiece, 0, 8)
+
+	for i := range pieces {
+		p, err := NewChessPiece(Queen, toChessPos(i+1, pieces[i]))
+		if err != nil {
+			panic(err)
+		}
+
+		chessPieces = append(chessPieces, *p)
+	}
+
+	board := NewChessBoard(60, chessPieces)
+	img, err := board.Draw()
+	if err != nil {
+		t.Fatal(err)
+	}
+	f, err := os.Create("8-queen-nw-solution.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	err = png.Encode(f, img)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func BenchmarkTheEightQueensProblemNW(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_ = TheEightQueensProblemNW()
+	}
+}
+
+func BenchmarkTheEightQueensProblemMySolution(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_ = TheEightQueensProblemMySolution()
+	}
+}
