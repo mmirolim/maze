6bb4148157f61cd3edc74d01fe42c75d3be20a37
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2019-04-30 22:58:58 +0500

    remove main

--- maze.go
+++ maze.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"image"
 	"image/color"
 	"image/color/palette"
 	"image/gif"
 	"math/rand"
-	"os"
 	"strings"
-	"time"
 )
 
 var (
-	w      = flag.Int("w", 10, "width of maze")
-	h      = flag.Int("h", 10, "height of maze")
 	red    = color.RGBA{R: 255, A: 255}
 	blue   = color.RGBA{B: 255, A: 255}
 	green  = color.RGBA{G: 255, A: 255}
@@ -23,86 +17,6 @@ var (
 	white  = color.RGBA{255, 255, 255, 255}
 	yellow = color.RGBA{255, 255, 102, 255}
 )
-
-func main() {
-	flag.Parse()
-	flag.PrintDefaults()
-	stack := NewStack()
-	start := time.Now()
-	cw, ch, ww := 20, 20, 5
-
-	maze, path := NewMaze(*w, *h, point{0, 0}, point{*w - 1, *h - 1},
-		DFS(stack, time.Now().Unix()),
-	)
-
-	fmt.Printf("Gen Time %v\n", time.Since(start))
-	maze.ResetVisitedCells()
-
-	start = time.Now()
-	fmt.Println(maze)
-	fmt.Printf("DrawTxt Time %v\n", time.Since(start))
-
-	start = time.Now()
-	mazeImg := &gif.GIF{
-		Image: []*image.Paletted{Draw(maze, white, black, cw, ch, ww)},
-		Delay: []int{10},
-	}
-	fmt.Printf("Draw Maze Time %v\n", time.Since(start))
-
-	start = time.Now()
-	mazeAnimGen := AnimatePath(maze, nil, path, nil, white, black, cw, ch, ww, 10)
-	fmt.Printf("Draw Maze Gen Time %v\n", time.Since(start))
-
-	mazePath := make([]*cell, 0, *w)
-	visited := make([]*cell, 0, *w)
-
-	start = time.Now()
-	isPathFound := FindPath(maze, maze.Begin(), maze.End(), &mazePath, &visited)
-	fmt.Printf("PathFound? with Recursion %v cells visited %d, path len %d Time %+v\n",
-		isPathFound, len(visited), len(mazePath), time.Since(start))
-	maze.ResetVisitedCells()
-
-	start = time.Now()
-	mazeAnimPath := AnimatePath(maze, visited, mazePath, green, blue, black, cw, ch, ww, 10)
-	fmt.Printf("Draw Maze Find Path Time %v\n", time.Since(start))
-
-	mazePath = mazePath[:0]
-	visited = visited[:0]
-
-	start = time.Now()
-	isPathFound = FindShortestPath(maze, maze.Begin(), maze.End(), &mazePath, &visited)
-	fmt.Printf("ShortestPathFound? with BFS %v cells visited %d path len %d Time %+v\n",
-		isPathFound, len(visited), len(mazePath), time.Since(start))
-	maze.ResetVisitedCells()
-
-	start = time.Now()
-	mazeAnimShortPath := AnimatePath(maze, visited, mazePath, yellow, red, black, cw, ch, ww, 10)
-	fmt.Printf("Draw Maze Find Shortest Path Time %v\n", time.Since(start))
-
-	// use maze as bg
-	mazeAnimPath.Image[0] = mazeImg.Image[0]
-	mazeAnimShortPath.Image[0] = mazeImg.Image[0]
-
-	data := []struct {
-		name string
-		img  *gif.GIF
-	}{
-		{"maze.gif", mazeImg},
-		{"maze-anim-gen-with-dfs.gif", mazeAnimGen},
-		{"maze-anim-path-with-dfs-recur.gif", mazeAnimPath},
-		{"maze-anim-path-with-bfs-weight.gif", mazeAnimShortPath},
-	}
-
-	var f *os.File
-	var err error
-	for _, img := range data {
-		if f, err = os.Create(img.name); err == nil {
-			if err = gif.EncodeAll(f, img.img); err != nil {
-				panic(err)
-			}
-		}
-	}
-}
 
 func DFS(stack *stack, seed int64) func(*Maze) (*Maze, []*cell) {
 	return func(m *Maze) (*Maze, []*cell) {
